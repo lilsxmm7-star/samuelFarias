0c36728f1c49b5ec98acdd5a908d36f6af0b4fe3
lilsxmm7-star <lilsxmm7@gmail.com>
2026-02-25 10:48:44 -0300

    Create exercicio9.js

---
+++ exercicio9.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Este método retorna o logaritmo natural de um número (base E).</title>
+</head>
+Autor: Samuel
+
+<body>
+  <script>
+    var valor1 = Math.log (2.2);
+    alert (valor1);
+  </script>
+</body>
+</html>
