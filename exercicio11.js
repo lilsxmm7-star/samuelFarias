d341d68135f135299c91452bc9da09ef8d5c994b
lilsxmm7-star <lilsxmm7@gmail.com>
2026-02-25 10:51:08 -0300

    Create exercicio11.js

---
+++ exercicio11.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title> Este método retorna o menor valor entre dois números.</title>
+</head>
+Autor: Samuel
+
+<body>
+  <script>
+    var valor1 = Math.min ( 3,9);
+    alert (valor1);
+  </script>
+</body>
+</html>
