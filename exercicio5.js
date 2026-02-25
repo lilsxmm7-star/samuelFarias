4865dd40b32b987490c15e0612981e5702f9e461
lilsxmm7-star <lilsxmm7@gmail.com>
2026-02-25 10:41:53 -0300

    Create exercicio5.js

---
+++ exercicio5.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Veja que utilizamos o mesmo número do exemplo anterior, só que agora em modo negativo. </title>
+</head>
+Autor: Samuel
+
+<body>
+  <script>
+    var valor1 = Math.ceil (-14.6);
+    alert (valor1);
+  </script>
+</body>
+</html>
