12b4cf6cdfd9177cc5fb6dd587a4474eba80b9d5
lilsxmm7-star <lilsxmm7@gmail.com>
2026-02-25 10:58:17 -0300

    Create exercicio16.js

---
+++ exercicio16.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title> Retorna a raiz quadrada de um número.</title>
+</head>
+Autor: Samuel
+
+<body>
+  <script>
+    var valor1 = Math.sqrt (9);
+    alert (valor1);
+  </script>
+</body>
+</html>
